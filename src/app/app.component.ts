4301d662beb6cd6bbe0c1e163b721addd67961b8
Ali Qazipoor <qazipoor@gmail.com>
2022-08-25 17:32:31 +0300

    First commit\

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<button (click)="clickHandler()">{{ status }}</button>`,
+  styles: ['button { background-color: darkturquoise; color: #fff; border: 1px solid #fff; width: 60px; height: 30px; cursor: pointer; border-radius: 5px; }']
 })
 export class AppComponent {
   title = 'Angular-Toggle-Button';
+
+  status = 'ON';
+
+  clickHandler() {
+    this.status = this.status === 'ON' ? 'OFF' : 'ON';
+  }
 }
